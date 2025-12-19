5a994a0b32ce9d31e6fffc02762cdda7fdcac481
sarvast <ss897@gmail.com>
2025-12-19 12:37:46 +0530

    feat: complete website overhaul with docs and routing

---
+++ deploy.bat
@@ -0,0 +1,5 @@
+git init
+git add .
+git commit -m "feat: complete website overhaul with docs and routing"
+git remote add origin https://github.com/sarvast/Caloriv-web.git
+git push -u origin main --force
